b7e8857eefeffd39ad4899acf7f571f736e9abd9
Rosa Kang <therosessom@gmail.com>
2025-05-02 16:35:13 -0400

    update the tailwindcss

--- themes/therosessom/index.php
+++ themes/therosessom/index.php
@@ -1,28 +1,11 @@
 <?php get_header(); ?>
 
-<main class='bg-gray-100 h-screen flex items-center justify-center p-5'>
-  <div class='bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm'>
-    <div class='flex justify-between items-center'>
-      <div class='flex flex-col items-start'>
-        <span class='text-gray-600 font-semibold -mb-1'>In Transit</span>
-        <span class='text-4xl font-semibold'>Coolblue</span>
-      </div>
-      <div class='size-12 bg-orange-400 rounded-full'></div>
-    </div>
-      <div class='my-2 flex items-center gap-2'>
-        <span class='bg-red-500 px-2.5 py-1.5 rounded-full uppercase text-white text-xm font-medium'>Today</span>
-        <span>9:30 - 10:30</span>
-      </div>
-      <div class='relative'>
-        <div class='bg-gray-200 w-full h-2 rounded'></div>
-        <div class='absolute top-0 bg-emerald-200 w-2/3 h-2 rounded'></div>
-      </div>
-      <div class='flex justify-between items-center mt-2.5 text-gray-600'>
-        <span class='text-wrap'>Expected</span>
-        <span class='text-wrap'>Sorting Center</span>
-        <span class='text-wrap'>In Transit</span>
-        <span class='text-gray-400 text-wrap'>Delivered</span>
-      </div>
+<main class='bg-gradient-to-tr from-cyan-200 to-purple-300 h-screen flex items-center justify-center p-5'>
+  <div class='bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2'>
+    
+    <input class='w-full rounded-full h-10 bg-gray-200 pl-5 outline-none ring-1 ring-transparent focus:ring-orange-400 transition-shadow placeholder:drop-shadow' type="text" placeholder='Search here...' />
+    <button class='hover:scale-95 focus:scale-90 bg-black text-white px-2.5 py-2 rounded-full transition transform font-medium outline-none'>Search</button>
+
   </div>
 </main>
 
